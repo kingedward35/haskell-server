5b3b5020180fb06217f43f2a52ad48ac26bdddf6
Verlon Smith <vsmith3113@gmail.com>
2018-04-24 21:13:20 -0600

    Added some more links and endpoints that send JSON

--- src/server.hs
+++ src/server.hs
@@ -2,21 +2,53 @@
 {-# LANGUAGE QuasiQuotes       #-}
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 import           Yesod
 
-data Links = Links
+-- data Links = Links
+data App = App
 
-mkYesod "Links" [parseRoutes|
+mkYesod "App" [parseRoutes|
 / HomeR GET
 /page1 Page1R GET
 /page2 Page2R GET
+/test TestR GET
 |]
 
-instance Yesod Links
+myLayout :: Widget -> Handler Html
+myLayout widget = do
+    pc <- widgetToPageContent $ do
+        widget
+        toWidget [lucius| body { font-family: verdana } |]
+    withUrlRenderer
+        [hamlet|
+            $doctype 5
+            <html>
+                <head>
+                    <title>#{pageTitle pc}
+                    <meta charset=utf-8>
+                    ^{pageHead pc}
+                <body>
+                    <article>
+                        ^{pageBody pc}
+        |]
 
-getHomeR  = defaultLayout [whamlet|<a href=@{Page1R}>Go to page 1!|]
+instance Yesod App where
+    defaultLayout = myLayout
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout
+    [whamlet|
+        <p>Hello World!
+        <a href=@{Page1R}>Go to page 1!
+    |]
+
+
 getPage1R = defaultLayout [whamlet|<a href=@{Page2R}>Go to page 2!|]
 getPage2R = defaultLayout [whamlet|<a href=@{HomeR}>Go home!|]
+getTestR = return $ object ["msg" .= "Hello World" 
+                           , "another msg" .= "Keep it moving!!"
+                           ]
 
 main :: IO ()
-main = warp 3000 Links
+main = warp 3000 App
